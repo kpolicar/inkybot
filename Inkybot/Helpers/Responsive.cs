794d22baada106dedc8fb69c62eac9b3558558d7
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-03 18:33:02 +0100

    extract responsive rect logic to helper class

---
+++ Inkybot/Helpers/Responsive.cs
@@ -0,0 +1,111 @@
+using System.Drawing;
+using Tesseract;
+
+namespace Inkybot.Helpers
+{
+    public class Responsive
+    {
+        
+        const double perfectRatio = 0.8;
+
+        public struct RatioRect
+        {
+            public double x;
+            public double y;
+            public double width;
+            public double height;
+            
+            public RatioRect(double x, double y, double width, double height) {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+            }
+        }
+
+        public class Measurement
+        {
+            public Rect Rectangle;
+            public int Width;
+            public int Height;
+        }
+
+        public static Rectangle ResponsiveRectangle(Measurement measurement, int width, int height) {
+            var r = measurement.Rectangle;
+            var statsRect = PointsToRatioRect(r.X1, r.Y1, r.X2, r.Y2, measurement.Width, measurement.Height);
+            
+            return RatioRectToScreenRect(statsRect, width, height);
+        }
+
+        private static RatioRect PointsToRatioRect(int p1x, int p1y, int p2x, int p2y, int w, int h) {
+            
+            var ratio = 1f*h/w;
+            
+            double gameW = w;
+            var xOffset = 0d;
+            if (ratio < perfectRatio) {
+                gameW = h / perfectRatio;
+                xOffset = (w - gameW) / 2;
+                
+                p1x -= (int) xOffset;
+                p2x -= (int) xOffset;
+            }
+            
+            double gameH = h;
+            var yOffset = 0d;
+            if (ratio > perfectRatio) {
+                gameH = w * perfectRatio;
+                yOffset = (h - gameH) / 2;
+                
+                p1y -= (int) yOffset;
+                p2y -= (int) yOffset;
+            }
+            
+            //Debug.WriteLine($"gameW:{gameW}, xOffset:{xOffset}, w: {w}, p1x: {p1x}");
+
+
+            var p1x_ratio = 1d * p1x / gameW;
+            var p1y_ratio = 1d * p1y / gameH;
+            var rw = p2x - p1x;
+            var rh = p2y - p1y;
+            
+            var rw_ratio = 1d * rw / gameW;
+            var rh_ratio = 1d * rh / gameH;
+            
+            return new RatioRect(
+                p1x_ratio,
+                p1y_ratio,
+                rw_ratio,
+                rh_ratio
+            );
+        }
+
+        // Convert a ratio rect to the rectangle that will be drawn on screen (responsive)
+        private static Rectangle RatioRectToScreenRect(RatioRect bounds, int width, int height) {
+            var w = 1d * width;
+            var h = 1d * height;
+            var ratio = h/w;
+            
+            var gameH = h;
+            var yOffset = 0d;
+            if (ratio > perfectRatio) {
+                gameH = perfectRatio * w;
+                yOffset = (h - gameH) / 2;
+            }
+            
+            var gameW = w;
+            var xOffset = 0d;
+            if (ratio < perfectRatio) {
+                gameW = h / perfectRatio;
+                xOffset = (w - gameW) / 2;
+            }
+
+
+            return new Rectangle(
+                (int) (bounds.x*gameW + xOffset), 
+                (int) (bounds.y*gameH + yOffset),
+                (int) (bounds.width*gameW),
+                (int) (bounds.height*gameH));
+        }
+    }
+}
--- Inkybot/MainForm.cs
+++ Inkybot/MainForm.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using Inkybot.Api;
 using Inkybot.Contracts;
 using Inkybot.Events;
+using Inkybot.Helpers;
 using Inkybot.Services;
 using Tesseract;
 
@@ -58,135 +59,16 @@ namespace Inkybot
             }));
         }
         
-        //const double perfectRatio = 0.8;
-
-        public struct RatioRect
-        {
-            public double x;
-            public double y;
-            public double width;
-            public double height;
-            
-            public RatioRect(double x, double y, double width, double height) {
-                this.x = x;
-                this.y = y;
-                this.width = width;
-                this.height = height;
-            }
-        }
-
-        private RatioRect PointsToRatioRect(int p1x, int p1y, int p2x, int p2y, int w, int h) {
-            
-            var perfectRatio = 0.8d; // h/w
-            var ratio = 1f*h/w;
-            
-            double gameW = w;
-            var xOffset = 0d;
-            if (ratio < perfectRatio) {
-                gameW = h / perfectRatio;
-                xOffset = (w - gameW) / 2;
-                
-                p1x -= (int) xOffset;
-                p2x -= (int) xOffset;
-            }
-            
-            double gameH = h;
-            var yOffset = 0d;
-            if (ratio > perfectRatio) {
-                gameH = w * perfectRatio;
-                yOffset = (h - gameH) / 2;
-                
-                p1y -= (int) yOffset;
-                p2y -= (int) yOffset;
-            }
-            
-            //Debug.WriteLine($"gameW:{gameW}, xOffset:{xOffset}, w: {w}, p1x: {p1x}");
-
-
-            var p1x_ratio = 1d * p1x / gameW;
-            var p1y_ratio = 1d * p1y / gameH;
-            var rw = p2x - p1x;
-            var rh = p2y - p1y;
-            
-            var rw_ratio = 1d * rw / gameW;
-            var rh_ratio = 1d * rh / gameH;
-            
-            return new RatioRect(
-                p1x_ratio,
-                p1y_ratio,
-                rw_ratio,
-                rh_ratio
-                );
-        }
-
-        // Convert a ratio rect to the rectangle that will be drawn on screen (responsive)
-        private Rectangle RatioRectToScreenRect(RatioRect bounds) {
-            var dimensions = ocrIndicatorPanel.Size;
-            var w = 1d * dimensions.Width;
-            var h = 1d * dimensions.Height;
-            var perfectRatio = 0.8d; // h/w
-            var ratio = h/w;
-            
-            var gameH = h;
-            var yOffset = 0d;
-            if (ratio > perfectRatio) {
-                gameH = perfectRatio * w;
-                yOffset = (h - gameH) / 2;
-            }
-            
-            var gameW = w;
-            var xOffset = 0d;
-            if (ratio < perfectRatio) {
-                gameW = h / perfectRatio;
-                xOffset = (w - gameW) / 2;
-            }
-
-
-            return new Rectangle(
-                (int) (bounds.x*gameW + xOffset), 
-                (int) (bounds.y*gameH + yOffset),
-                (int) (bounds.width*gameW),
-                (int) (bounds.height*gameH));
-        }
-        
         private void paintOcrIndicators(object sender, EventArgs eventArgs) {
             var g = ocrIndicatorPanel.CreateGraphics();
-            var dimensions = ocrIndicatorPanel.Size;
-            var w = 1d * dimensions.Width;
-            var h = 1d * dimensions.Height;
-            var perfectRatio = 0.8d; // h/w
-            var ratio = h/w;
-            
-            
-            var gameH = h;
-            var yOffset = 0d;
-            if (ratio > perfectRatio) {
-                gameH = perfectRatio * w;
-                yOffset = (h - gameH) / 2;
-            }
-            
-            var gameW = w;
-            var xOffset = 0d;
-            if (ratio < perfectRatio) {
-                gameW = h / perfectRatio;
-                xOffset = (w - gameW) / 2;
-            }
-
 
             var pen = new Pen(Color.Red, 2);
-            //var statsRect = new Rectangle(
-            //    //(int) (0.35582*(w + xOffset)), 
-            //    (int) (0.24362*gameW + xOffset), 
-            //    (int) (0.29841*gameH + yOffset),
-            //    //(int) (0.1534*w),
-            //    (int) (0.2744*gameW),
-            //    (int) (0.53050*gameH));
 
-            var statBounds = DofusScreenScan.StatBounds;
-            var statsRect = PointsToRatioRect(187, 343, 398, 667, 777, 933);
-            //var statsRect = PointsToRatioRect(634, 303, 980, 842, 1920, 1017);
-            //var statsRect = PointsToRatioRect(302, 303, 638, 830, 1257, 1008);
-            var drawRect = RatioRectToScreenRect(statsRect);
+            var drawRect = Responsive.ResponsiveRectangle(new Responsive.Measurement {
+                Rectangle = Rect.FromCoords(187, 343, 398, 667),
+                Width =  777,
+                Height = 933
+            }, ocrIndicatorPanel.Width, ocrIndicatorPanel.Height);
             
             //var statsRect = new Rectangle(626, 300, 980 - 626, 39 * 14);
             g.DrawRectangle(pen, drawRect);
