b354143c6db3abfa7ef977450dcd086e55eb2e76
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-11 16:57:02 +0100

    add bottom panel with high sink stat restore checkbox and credits for ai stat config

--- Inkybot/ConfigForm.Designer.cs
+++ Inkybot/ConfigForm.Designer.cs
@@ -38,7 +38,12 @@ namespace Inkybot
             this.RaRuneThresholdColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MaxSmRuneCanHitColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MaxPaRuneCanHitColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.restoreHighSinkStatsCheckbox = new System.Windows.Forms.CheckBox();
+            this.creditsNameLabel = new System.Windows.Forms.Label();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.creditsLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize) (this.statsDataGridView)).BeginInit();
+            this.bottomPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // statsDataGridView
@@ -113,20 +118,81 @@ namespace Inkybot
             this.MaxPaRuneCanHitColumn.HeaderText = "Max (RA Rune)";
             this.MaxPaRuneCanHitColumn.Name = "MaxPaRuneCanHitColumn";
             // 
+            // restoreHighSinkStatsCheckbox
+            // 
+            this.restoreHighSinkStatsCheckbox.AutoSize = true;
+            this.restoreHighSinkStatsCheckbox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.restoreHighSinkStatsCheckbox.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.restoreHighSinkStatsCheckbox.ForeColor = System.Drawing.SystemColors.Control;
+            this.restoreHighSinkStatsCheckbox.Location = new System.Drawing.Point(15, 10);
+            this.restoreHighSinkStatsCheckbox.Name = "restoreHighSinkStatsCheckbox";
+            this.restoreHighSinkStatsCheckbox.Size = new System.Drawing.Size(194, 21);
+            this.restoreHighSinkStatsCheckbox.TabIndex = 0;
+            this.restoreHighSinkStatsCheckbox.Text = "Restore high sink stats immediately";
+            this.restoreHighSinkStatsCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // creditsNameLabel
+            // 
+            this.creditsNameLabel.AutoSize = true;
+            this.creditsNameLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.creditsNameLabel.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.creditsNameLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.creditsNameLabel.Location = new System.Drawing.Point(707, 10);
+            this.creditsNameLabel.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.creditsNameLabel.Name = "creditsNameLabel";
+            this.creditsNameLabel.Size = new System.Drawing.Size(53, 13);
+            this.creditsNameLabel.TabIndex = 1;
+            this.creditsNameLabel.Text = "Tomolone\r\n";
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.Controls.Add(this.creditsLabel);
+            this.bottomPanel.Controls.Add(this.creditsNameLabel);
+            this.bottomPanel.Controls.Add(this.restoreHighSinkStatsCheckbox);
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 551);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Padding = new System.Windows.Forms.Padding(15, 10, 15, 10);
+            this.bottomPanel.Size = new System.Drawing.Size(775, 41);
+            this.bottomPanel.TabIndex = 3;
+            // 
+            // creditsLabel
+            // 
+            this.creditsLabel.AutoSize = true;
+            this.creditsLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.creditsLabel.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.creditsLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.creditsLabel.Location = new System.Drawing.Point(596, 10);
+            this.creditsLabel.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.creditsLabel.Name = "creditsLabel";
+            this.creditsLabel.Size = new System.Drawing.Size(111, 13);
+            this.creditsLabel.TabIndex = 2;
+            this.creditsLabel.Text = "Configured with  ❤ ️ by";
+            // 
             // ConfigForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (30)))), ((int) (((byte) (30)))), ((int) (((byte) (30)))));
             this.ClientSize = new System.Drawing.Size(775, 592);
+            this.Controls.Add(this.bottomPanel);
             this.Controls.Add(this.statsDataGridView);
             this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
             this.Name = "ConfigForm";
             this.Text = "Config - Inkybot";
             this.Load += new System.EventHandler(this.ConfigForm_OnLoad);
             ((System.ComponentModel.ISupportInitialize) (this.statsDataGridView)).EndInit();
+            this.bottomPanel.ResumeLayout(false);
+            this.bottomPanel.PerformLayout();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Panel bottomPanel;
+
+        private System.Windows.Forms.Label creditsLabel;
+        private System.Windows.Forms.Label creditsNameLabel;
+
+        private System.Windows.Forms.CheckBox restoreHighSinkStatsCheckbox;
 
         private System.Windows.Forms.DataGridViewTextBoxColumn MaxPaRuneCanHitColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn MaxSmRuneCanHitColumn;
