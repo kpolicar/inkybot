5e6e7b07c8e9420546fbc1fbec9ab7b9f09701aa
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-12-31 13:04:09 +0100

    obfuscate api resources

--- Inkybot/Api/Resources/FreeTrial.cs
+++ Inkybot/Api/Resources/FreeTrial.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Reflection;
 
 namespace Inkybot.Api.Resources
 {
     #pragma warning disable 8618
-    [Serializable]
     public class FreeTrial
     {
-        public bool expired;
-        public DateTime? created_at;
-        public DateTime? updated_at;
+        public bool expired => k3ExasHrvpLP4Rm;
+        [Obfuscation(Exclude = true)]
+        public bool k3ExasHrvpLP4Rm;
+        
+        public DateTime? created_at => Ly7lqp8846XaG7P;
+        [Obfuscation(Exclude = true)]
+        public DateTime? Ly7lqp8846XaG7P;
     }
     #pragma warning restore 8618
 }
--- Inkybot/Api/Resources/User.cs
+++ Inkybot/Api/Resources/User.cs
@@ -1,18 +1,34 @@
 using System;
+using System.Reflection;
 
 namespace Inkybot.Api.Resources
 {
     #pragma warning disable 8618
-    [Serializable]
     public class User
     {
-        public string email;
-        public bool is_subscribed;
-        public bool is_free_trial;
-        public bool free_trial_available;
-        public string name;
-        public string password;
-        public DateTime? subscribed_to;
+        public string email => CXpD6X71WZhYsHf;
+        [Obfuscation(Exclude = true)]
+        public string CXpD6X71WZhYsHf;
+        
+        public bool is_subscribed => wVakGMaAnUQkCFZ;
+        [Obfuscation(Exclude = true)]
+        public bool wVakGMaAnUQkCFZ;
+        
+        public bool is_free_trial => Sw6mNjvR0HZofKj;
+        [Obfuscation(Exclude = true)]
+        public bool Sw6mNjvR0HZofKj;
+        
+        public bool free_trial_available => EbP8tMjESR6IGvi;
+        [Obfuscation(Exclude = true)]
+        public bool EbP8tMjESR6IGvi;
+        
+        public string name => i30jfVx9krmacQH;
+        [Obfuscation(Exclude = true)]
+        public string i30jfVx9krmacQH;
+
+        public DateTime? subscribed_to => rbvQL1e41MOgDLA;
+        [Obfuscation(Exclude = true)]
+        public DateTime? rbvQL1e41MOgDLA;
     }
     #pragma warning restore 8618
 }
