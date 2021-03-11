f7bb8d0dfb55a5d7cdebc73e9a0a4b603e948fdd
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-11 16:19:24 +0100

    better enforce that item has changed (if it should have)

--- Inkybot/Services/Tick.cs
+++ Inkybot/Services/Tick.cs
@@ -305,10 +305,12 @@ namespace Inkybot.Services
 
             private void EnforceStatsChanged(Item item) {
                 if (job.state.PreviousItem != null) {
-                    var areDifferent =
-                        job.state.PreviousHistory?.history.First().Landed == null ||
-                        item.HasDifferentStatValues(job.state.PreviousItem);
-                
+                    
+                    var shouldBeDifferent = job.state.PreviousHistory?.history.First() != MageHistoryRecord.Failure;
+                    if (!shouldBeDifferent)
+                        return;
+                    
+                    var areDifferent = item.HasDifferentStatValues(job.state.PreviousItem);
                     if (!areDifferent)
                         throw new UnexpectedMageResultException("Expected Stats to change but didn't");
                 }
