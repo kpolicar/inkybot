a82ca3e156159ca437013190ae940e3787c943fd
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-11 18:51:55 +0100

    rune checking true by default

--- Inkybot/Properties/Settings.Designer.cs
+++ Inkybot/Properties/Settings.Designer.cs
@@ -121,7 +121,7 @@ namespace Inkybot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool enableRuneChecking {
             get {
                 return ((bool)(this["enableRuneChecking"]));
