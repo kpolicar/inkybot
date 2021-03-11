2340f9fbeca8e5aedeebcf5c0798b8a9afacca56
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-11 18:17:17 +0100

    revert item has changed check

--- Inkybot/Services/Tick.cs
+++ Inkybot/Services/Tick.cs
@@ -305,12 +305,10 @@ namespace Inkybot.Services
 
             private void EnforceStatsChanged(Item item) {
                 if (job.state.PreviousItem != null) {
-                    
-                    var shouldBeDifferent = job.state.PreviousHistory?.history.First() != MageHistoryRecord.Failure;
-                    if (!shouldBeDifferent)
-                        return;
-                    
-                    var areDifferent = item.HasDifferentStatValues(job.state.PreviousItem);
+                    var areDifferent =
+                        job.state.PreviousHistory?.history.First().Landed == null ||
+                        item.HasDifferentStatValues(job.state.PreviousItem);
+                
                     if (!areDifferent)
                         throw new UnexpectedMageResultException("Expected Stats to change but didn't");
                 }
