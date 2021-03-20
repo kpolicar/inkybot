505463645e5bb8161d80d344c44a3aeec0e572bd
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-20 10:46:45 +0100

    version request is not encrypted

--- Inkybot/Api/ApiClient.cs
+++ Inkybot/Api/ApiClient.cs
@@ -71,7 +71,7 @@ namespace Inkybot.Api
             var client = new HttpClient();
             var response = await client.GetAsync(Server.ApiUrl);
             response.EnsureSuccessStatusCode();
-            var result = await GetResultFromEncryptedResponse(response);
+            var result = await response.Content.ReadAsStringAsync();
 
             Debug.WriteLine("Http response: "+result);
             return JsonConvert.DeserializeObject<VersionDetails>(result);
