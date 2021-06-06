a6543107ceb69d2c67b511ad04a38be69a9a733d
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-06-06 18:49:58 +0200

    use digits data pack as well as the language pack

--- Inkybot/Services/ScreenScanner.cs
+++ Inkybot/Services/ScreenScanner.cs
@@ -46,7 +46,7 @@ namespace Inkybot.Services
             protected virtual TesseractEngine CreateEngine() {
                 return new TesseractEngine(
                     Path.Combine(AppContext.BaseDirectory, @"Resources\Tesseract"),
-                    CultureInfo.CurrentUICulture.ThreeLetterISOLanguageName,
+                    CultureInfo.CurrentUICulture.ThreeLetterISOLanguageName+"+digits",
                     EngineMode.Default);
             }
 
