5485db71eadb87c6699ee4b26128f784396906c7
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-09-13 13:52:08 +0200

    stop maging if queue changes (if the first item in the queue changed)

--- Inkybot/MageQueueForm.cs
+++ Inkybot/MageQueueForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Inkybot.Contracts;
 using Inkybot.Dofus.Contracts;
 using Inkybot.Events;
 using Inkybot.Resources;
@@ -22,12 +23,14 @@ namespace Inkybot
         
         private MageQueueManager mageQueue;
         private ConfigManager configManager;
+        private DofusMagingJob magingJob;
 
 
         public MageQueueForm() {
             InitializeComponent();
             configManager = (ConfigManager) Program.Services.GetService<MageConfigManager>();
             mageQueue = Program.Services.GetService<MageQueueManager>();
+            magingJob = Program.Services.GetService<DofusMagingJob>();
             mageQueue.Enqueued += OnMagingEnqueued;
             mageQueue.Dequeued += OnMagingDequeuedOrRemoved;
             mageQueue.Removed += OnMagingDequeuedOrRemoved;
@@ -39,13 +42,16 @@ namespace Inkybot
             mageQueueGroupBoxesPanel.Visible = !mageQueue.Empty;
         }
 
-        private void OnMagingMoved(object sender, MageQueueMovedEventArgs e) =>
+        private void OnMagingMoved(object sender, MageQueueMovedEventArgs e) {
+            if (mageQueue.Peek() == e.QueueItem || e.Index == 0)
+                magingJob.StopMage();
             BeginInvoke(new MethodInvoker(() => {
                 var groupBox = mageQueueGroupBoxes[e.QueueItem];
                 mageQueueGroupBoxesPanel.Controls.SetChildIndex(groupBox, e.Index);
             }));
+        }
 
-        private void OnMagingEnqueued(object sender, MageQueueEventArgs e) =>
+        private void OnMagingEnqueued(object sender, MageQueueEventArgs e) {
             BeginInvoke(new MethodInvoker(() => {
                 var control = BuildMageQueueGroupBox(e.QueueItem);
                 SuspendLayout();
@@ -54,8 +60,13 @@ namespace Inkybot
                 mageQueueGroupBoxes[e.QueueItem] = control;
                 UpdateControlsVisibility();
             }));
-        
-        private void OnMagingDequeuedOrRemoved(object sender, MageQueueEventArgs e) =>
+            if (mageQueue.Peek() == e.QueueItem)
+                magingJob.StopMage();
+        }
+
+        private void OnMagingDequeuedOrRemoved(object sender, MageQueueEventArgs e) {
+            if (mageQueue.Peek() == e.QueueItem)
+                magingJob.StopMage();
             BeginInvoke(new MethodInvoker(() => {
                 SuspendLayout();
                 mageQueueGroupBoxesPanel.Controls.Remove(mageQueueGroupBoxes[e.QueueItem]);
@@ -63,15 +74,25 @@ namespace Inkybot
                 mageQueueGroupBoxes.Remove(e.QueueItem);
                 UpdateControlsVisibility();
             }));
+        }
 
-        private void OnMageQueueItemRemove(object sender, MageQueueEventArgs e) =>
+        private void OnMageQueueItemRemove(object sender, MageQueueEventArgs e) {
+            if (mageQueue.Peek() == e.QueueItem)
+                magingJob.StopMage();
             mageQueue.Remove(e.QueueItem);
+        }
 
-        private void OnMageQueueItemMoveUp(object sender, MageQueueEventArgs e) =>
+        private void OnMageQueueItemMoveUp(object sender, MageQueueEventArgs e) {
             mageQueue.MoveForward(e.QueueItem);
+            if (mageQueue.Peek() == e.QueueItem)
+                magingJob.StopMage();
+        }
 
-        private void OnMageQueueItemMoveDown(object sender, MageQueueEventArgs e) =>
+        private void OnMageQueueItemMoveDown(object sender, MageQueueEventArgs e) {
+            if (mageQueue.Peek() == e.QueueItem)
+                magingJob.StopMage();
             mageQueue.MoveBack(e.QueueItem);
+        }
         
         private void MageQueueForm_Closing(object sender, CancelEventArgs cancelEventArgs) {
             cancelEventArgs.Cancel = true;
@@ -81,11 +102,17 @@ namespace Inkybot
         private void OnMageQueueStatPresetSelectedIndexChanged(object sender, MageQueueEventArgs e, int selectedIndex) {
             var mage = mageQueue.Queue.Find(item => item == e.QueueItem);
             mage.Config.PresetIndex = selectedIndex != 0 ? selectedIndex-1 : null;
+            
+            if (mageQueue.Peek() == e.QueueItem)
+                magingJob.StopMage();
         }
 
         private void OnMageQueueConfigPresetSelectedIndexChanged(object sender, MageQueueEventArgs e, int selectedIndex) {
             var mage = mageQueue.Queue.Find(item => item == e.QueueItem);
             mage.Config.ConfigPresetIndex = selectedIndex != 0 ? selectedIndex-1 : null;
+            
+            if (mageQueue.Peek() == e.QueueItem)
+                magingJob.StopMage();
         }
 
         public async Task Highlight(MageQueueManager.MageQueueItem mageQueueItem, int delay=800) {
