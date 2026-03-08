9982fa01a4927b85acd10082aa49e8d5b666ad82
Klemen Poličar <klemen.janez.policar@gmail.com>
2026-03-08 15:27:20 +0100

    better error handling in case of odd screenshots

--- Inkybot/Services/Tick.cs
+++ Inkybot/Services/Tick.cs
@@ -92,7 +92,7 @@ namespace Inkybot.Services
                     job.Warning?.Invoke(this, new MagingJobErrorEventArgs(exception, $"Attempt #{StatsChangedChecksCount} out of ${MaxStatsChangedChecks}"));
                     job.dataProvider.FetchData();
 
-                    Thread.Sleep(100);
+                    Thread.Sleep(500);
                     return;
                 }
 
